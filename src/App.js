223d1a9904a415cb6ba8246fac82bbea11dcbdfb
Nevaland <deanh716@gmail.com>
2021-01-23 11:48:25 +0900

    Implement sidebar ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react"
-import './App.css';
-import Sidebar from './Sidebar';
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,28 +1,73 @@
-import React from 'react'
-import './Sidebar.css'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import AddIcon from '@material-ui/icons/Add';
+import React from "react";
+import "./Sidebar.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import SidebarChannel from "./SidebarChannel";
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import CallIcon from "@material-ui/icons/Call";
+import { Avatar } from "@material-ui/core";
+import MicIcon from "@material-ui/icons/Mic";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <div className="sidebar__top">
-                <h3>Nevaland</h3>
-                <ExpandMoreIcon />
-            </div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Nevaland</h3>
+        <ExpandMoreIcon />
+      </div>
 
-            <div className="sidebar__channels">
-                <div className="sidebar__channelsHeader">
-                    <div className="sidebar__header">
-                        <ExpandMoreIcon />
-                        <h4>Text Channels</h4>
-                    </div>
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
+          </div>
 
-                    <AddIcon className="sidebar__addChannel" />
-                </div>
-            </div>
+          <AddIcon className="sidebar__addChannel" />
         </div>
-    )
+
+        <div className="sidebar__channelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+      </div>
+
+      <div className="sidebar__voice">
+        <SignalCellularAltIcon
+          className="sidebar__voiceIcon"
+          fontSize="large"
+        />
+        <div className="sidebar__voiceInfo">
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
+        </div>
+
+        <div className="sidebar__voiceIcons">
+          <InfoOutlinedIcon />
+          <CallIcon />
+        </div>
+      </div>
+
+      <div className="sidebar__profile">
+        <Avatar />
+        <div className="sidebar__profileInfo">
+          <h3>@neva</h3>
+          <p>#thisIsMyId</p>
+        </div>
+
+        <div className="sidebar__profileIcons">
+          <MicIcon />
+          <HeadsetIcon />
+          <SettingsIcon />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
