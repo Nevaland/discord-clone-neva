c9233eb2ee25b490c073b75ea33df52bdff5973c
Nevaland <deanh0716@gmail.com>
2021-01-23 16:42:45 +0900

    Add chat space

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./App.css";
+import Chat from "./Chat";
 import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-      {/* Sidebar */}
       <Sidebar />
-
-      {/* Chat */}
+      <Chat />
     </div>
   );
 }
