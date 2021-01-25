6b665eeca814161e69224d16a3a2d6856d65d24f
Nevaland <deanh0716@gmail.com>
2021-01-25 17:17:48 +0900

    Add dispatch of login, logout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Chat from "./Chat";
 import { selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
 import Login from "./Login";
 import Sidebar from "./Sidebar";
+import { login, logout } from "./features/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("user is ", authUser);
+      if (authUser) {
+        // the user is logged in
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        // the user is logged out
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div className="app">
       {user ? (
