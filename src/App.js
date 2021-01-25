09ce65485a5bfb2f7d823ed092fa8e36914f6f14
Nevaland <deanh0716@gmail.com>
2021-01-25 22:51:59 +0900

    Remove debug print

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
 
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
-      console.log("user is ", authUser);
       if (authUser) {
         // the user is logged in
         dispatch(
