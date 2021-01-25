2b033fca0e46d5be5d00c08b19d45894e66e52df
Nevaland <deanh0716@gmail.com>
2021-01-25 17:16:35 +0900

    Implement signin function

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,12 @@
 import { Button } from "@material-ui/core";
+import { auth, provider } from "./firebase";
 import React from "react";
 import "./Login.css";
 
 function Login() {
-  const signIn = () => {};
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  };
   return (
     <div className="login">
       <div className="login__logo">
