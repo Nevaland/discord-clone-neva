b9561669785408678c7b05c26a2e57132e71ccc8
Nevaland <deanh0716@gmail.com>
2021-01-25 23:02:29 +0900

    Implement message

--- src/Message.js
+++ src/Message.js
@@ -2,17 +2,19 @@ import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./Message.css";
 
-function Message() {
+function Message({ timestamp, message, user }) {
   return (
     <div className="message">
-      <Avatar />
+      <Avatar src={user.photo} />
       <div className="message__info">
         <h4>
-          NEVA
-          <span className="message__timestamp">timestamp</span>
+          {user.displayName}
+          <span className="message__timestamp">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
         </h4>
 
-        <p>This is message</p>
+        <p>{message}</p>
       </div>
     </div>
   );
