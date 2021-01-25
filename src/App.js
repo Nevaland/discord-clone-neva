b439abff7ddc10ac280e072f6d45d41e27979282
Nevaland <deanh0716@gmail.com>
2021-01-25 11:43:11 +0900

    Add login page and branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Chat from "./Chat";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
 import Sidebar from "./Sidebar";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,21 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  const signIn = () => {};
+  return (
+    <div className="login">
+      <div className="login__logo">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/sco/thumb/9/98/Discord_logo.svg/1200px-Discord_logo.svg.png"
+          alt=""
+        />
+      </div>
+
+      <Button onClick={signIn}>Sign In</Button>
+    </div>
+  );
+}
+
+export default Login;
